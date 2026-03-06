2cf5dd68f2d3cd471f567863ee722ec7ff15f272
szami-dev <145538960+szami-dev@users.noreply.github.com>
2026-03-06 09:02:40 +0100

    next-start

---
+++ next-app/app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to OptikArt</h1>
+      <Link href="/sign-up">Sign Up</Link>
+    </div>
+  );
+}
