152dfea965eb378f6d8d9d29905459a1398b1887
szami-dev <145538960+szami-dev@users.noreply.github.com>
2026-03-17 09:04:20 +0100

    sajt

--- nextjs-docker/prisma.config.ts
+++ nextjs-docker/prisma.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
+    seed: 'npx tsx ./prisma/seed.ts', // ts-node helyett tsx
     path: "prisma/migrations",
   },
   datasource: {
