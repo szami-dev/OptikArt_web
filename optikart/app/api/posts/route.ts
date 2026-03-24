3442698261c4da40864e2e335071f38b40d8807c
szami-dev <145538960+szami-dev@users.noreply.github.com>
2026-03-24 09:09:40 +0100

    123

--- optikart/app/api/posts/route.ts
+++
@@ -1,8 +0,0 @@
-import prisma from "@/lib/prisma";
-
-export async function GET() {
-  
-  return new Response(JSON.stringify("alma"), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
