e9b865fa3e019e0b38bd5936c370f2b551470ce7
szami-dev <145538960+szami-dev@users.noreply.github.com>
2026-04-19 23:06:31 +0200

    11

--- lib/email.ts
+++ lib/email.ts
@@ -5,7 +5,7 @@ import { randomUUID } from "crypto";
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SERVER_HOST,
   port: Number(process.env.EMAIL_SERVER_PORT),
-  secure: process.env.EMAIL_SERVER_PORT === "587",
+  secure: false,
   auth: {
     user: process.env.EMAIL_SERVER_USER,
     pass: process.env.EMAIL_SERVER_PASSWORD,
